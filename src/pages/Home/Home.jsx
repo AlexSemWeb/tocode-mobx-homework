0a161fcf99a8fc1d08094eb75fd5d82747de5fba
Evgenii Vedegis <tichoo@yandex.ru>
2022-05-11 19:55:33 +0800

    added nextRes (new array from response)

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,15 +9,30 @@ const store = new threadsStore()
 import { Container } from 'layouts'
 
 const HomePage = () => {
-  fetchReddit
-    .get()
-    .then((res) => console.log(res))
-    .catch((err) => console.log(err))
-    .finally(() => console.log('done'))
+  const [arr, setArr] = React.useState([])
+
+  React.useEffect(() => {
+    fetchReddit
+      .get()
+      .then((res) => setArr(res))
+      .catch((err) => console.log(err))
+      .finally(() => console.log('done'))
+  }, [])
 
   return (
     <Container>
       <h1 className='ui-title-1'>Home Page</h1>
+      {/* {arr &&
+        arr.data &&
+        arr.data.children.map((item) => <p>{item.data.title}</p>)} */}
+
+      {arr &&
+        arr.length > 0 &&
+        arr.map((item) => (
+          <a key={item.id} href={item.url}>
+            {item.title}
+          </a>
+        ))}
     </Container>
   )
 }
--- src/utils/fetchReddit.js
+++ src/utils/fetchReddit.js
@@ -7,7 +7,21 @@ export default {
   // GET
   async get() {
     const res = await getData('https://www.reddit.com/r/reactjs.json')
-    return res
+    const resChildren = await res.data.children
+
+    const nextRes = []
+    resChildren.forEach((el) => {
+      nextRes.push({
+        id: el.data.id,
+        title: el.data.title,
+        url: el.data.url,
+      })
+      // nextRes.push(el.data)
+    })
+
+    console.log('original', res)
+    console.log('next', nextRes)
+    return nextRes
   },
   // PUT
   // update(id, item) {},
