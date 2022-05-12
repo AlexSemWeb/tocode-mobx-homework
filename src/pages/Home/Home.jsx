87b6505b2fcb63f3ac09536b8e23f00ba077670d
Evgenii Vedegis <tichoo@yandex.ru>
2022-05-12 19:03:41 +0800

    added status/state; moved logic to the store

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import fetchReddit from 'utils/fetchReddit'
+import { observer } from 'mobx-react-lite'
 
 // store
 import threadsStore from 'store/threadsStore'
@@ -8,33 +8,35 @@ const store = new threadsStore()
 // components
 import { Container } from 'layouts'
 
-const HomePage = () => {
-  const [arr, setArr] = React.useState([])
+const HomePage = observer(() => {
+  React.useEffect(() => store.fetchThreads(), [])
 
-  React.useEffect(() => {
-    fetchReddit
-      .get()
-      .then((res) => setArr(res))
-      .catch((err) => console.log(err))
-      .finally(() => console.log('done'))
-  }, [])
-
-  return (
-    <Container>
-      <h1 className='ui-title-1'>Home Page</h1>
-      {/* {arr &&
-        arr.data &&
-        arr.data.children.map((item) => <p>{item.data.title}</p>)} */}
-
-      {arr &&
-        arr.length > 0 &&
-        arr.map((item) => (
+  // error
+  const renderError = store.status === 'error' && <p>connection error</p>
+  // loading
+  const renderLoading = store.status === 'pending' && <p>loading ....</p>
+  // content
+  const renderContent = store.status === 'done' && (
+    <div>
+      {store.threads &&
+        store.threads.length > 0 &&
+        store.threads.map((item) => (
           <a key={item.id} href={item.url}>
             {item.title}
           </a>
         ))}
+    </div>
+  )
+
+  return (
+    <Container>
+      <h1 className='ui-title-1'>Home Page</h1>
+
+      {renderError}
+      {renderLoading}
+      {renderContent}
     </Container>
   )
-}
+})
 
 export default HomePage
--- src/store/threadsStore.js
+++ src/store/threadsStore.js
@@ -2,15 +2,29 @@
 // threadsStore.js
 
 import { makeAutoObservable } from 'mobx'
+import fetchReddit from 'utils/fetchReddit'
 
 class threadsStore {
   threads = null
+  status = 'pending' // pending done error
 
   constructor() {
     makeAutoObservable(this)
   }
 
-  fetchThreads() {}
+  fetchThreads() {
+    !this.threads &&
+      fetchReddit
+        .get()
+        .then((res) => {
+          this.threads = res
+          this.status = 'done'
+        })
+        .catch((err) => {
+          this.status = 'error'
+          console.log(err)
+        })
+  }
 }
 
 export default threadsStore
